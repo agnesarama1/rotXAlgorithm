5568082afffcea27080f3f95ddc3c73958246768
agnesarama1 <agnesa.rama1@student.uni-pr.edu>
2024-04-18 17:14:14 +0200

    Encryption Function

--- EncryptionExample.java
+++ EncryptionExample.java
@@ -1,3 +1,18 @@
-public class EncryptionExample {
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Scanner;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 
+public class EncryptionExample {
+    private static final String ALGORITHM = "AES/ECB/PKCS5Padding";
+    private static final String ROTX_ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public static String encrypt(String message, Key key) throws Exception {
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encryptedBytes = cipher.doFinal(message.getBytes("UTF-8"));
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
 }
