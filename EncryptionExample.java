ac885a95f26bc0fc6dc7034434885f91a668ec9d
Agnesamani <150405608+Agnesamani@users.noreply.github.com>
2024-04-18 19:39:57 +0200

    Decryption Function

--- EncryptionExample.java
+++ EncryptionExample.java
@@ -15,4 +15,10 @@ public class EncryptionExample {
         byte[] encryptedBytes = cipher.doFinal(message.getBytes("UTF-8"));
         return Base64.getEncoder().encodeToString(encryptedBytes);
     }
+    public static String decrypt(String encryptedMessage, Key key) throws Exception {
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedMessage));
+        return new String(decryptedBytes, "UTF-8");
+    }
 }
